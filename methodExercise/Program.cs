6f04d6a86c6b1af350dc85d8bc95b5a51eb9f9a6
Grayson Owen <graysonowen@Graysons-MacBook-Pro.local>
2020-06-19 13:16:26 -0500

    Methods Exc.2

--- methodExercise/Program.cs
+++ methodExercise/Program.cs
@@ -4,9 +4,49 @@ namespace methodExercise
 {
     class Program
     {
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+
+            var amountofturkeys = Sum(3, 8);
+
+
+
+
+
+            //----------------------Exercise 1--------------
+            //Name: Jeb
+            //Favorite Food: Snack
+            //Favorite Animal: Mouse
+            //Favorite Hobby: Sleeping
+
+            Console.WriteLine("Hello ya dangus, What's your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Nice to see you, {userName} what is you favorite food?");
+            var food = Console.ReadLine();
+
+            Console.WriteLine($"{food} is amazing. What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"{animal} are such little cuties. What do you like to do?");
+            var hobby = Console.ReadLine();
+
+            Console.WriteLine($"Thanks, {userName}! here is your profile");
+            Console.WriteLine($"------------------------------------");
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite Food: {food}");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine($"Favorite Hobby: {hobby}");
+
+            //----------------------Exercise 2-------------
+
+
+        }   
     }
 }
